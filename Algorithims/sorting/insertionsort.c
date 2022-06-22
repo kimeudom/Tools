e42276404f7a23cad4b3bc9a40a1dbc5d4bdf340
Dominic <>
2022-06-22 19:13:42 +0300

    insertion sort stripped

--- Algorithims/sorting/insertionsort.c
+++ Algorithims/sorting/insertionsort.c
@@ -1,7 +1,14 @@
-// Insertion sort algorithim implementation
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main(int argc, char **argv) { return 0; }
+// The insertion sort algorithim
+void insertionSort(float array[], int size) {
+  for (int i = 0; i < size; i++) {
+    float currentValue = array[i];
+    int j = i - 1;
+    while (j >= 0 && array[j] > array[j + 1]) {
+      array[j + 1] = array[j];
+      j -= 1;
+    }
+    array[j + 1] = currentValue;
+  }
+}
